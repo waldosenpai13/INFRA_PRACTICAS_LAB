6e6f5d404e12c90893f68650d4824eedea7fb7a8
WalCodexpe <132695259+waldosenpai13@users.noreply.github.com>
2026-04-15 09:54:06 -0500

    Update variables.tf

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "region" {
+  description = "Región de AWS"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "project_name" {
+  description = "Nombre del proyecto"
+  type        = string
+  default     = "mi-proyecto-iac"
+}
